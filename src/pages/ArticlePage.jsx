6368fa93fdaa0904e39df8177b4c7006a0dbf437
Daniel Rojas <daniel.drojas@outlook.com>
2020-12-02 15:53:43 -0500

    add params to article page

---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import articleContent from './article-content';
+
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name)
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => (
+            <p>{paragraph}</p>
+            ))}
+            
+            
+        </>
+    );
+};
+
+export default ArticlePage;

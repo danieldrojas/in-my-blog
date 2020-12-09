f8f5257315ad4f87dfbc631d8b8cc3fee626371a
Daniel Rojas <daniel.drojas@outlook.com>
2020-12-09 13:47:03 -0500

    server doesnt render static fiels

--- in-my-blog-backend/src/server.js
+++ in-my-blog-backend/src/server.js
@@ -1,10 +1,32 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb'
+import { MongoClient } from 'mongodb';
+import path from 'path';
 
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const withDB = async (operations, res) => {
     try {
@@ -46,7 +68,7 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
     const articleName = req.params.name;
     withDB(async (db) => {
         const articleInfo = await db.collection('articles').findOne({ name: articleName });
-        await db.collection('articles').updateOne({ name: articleName },{
+        await db.collection('articles').updateOne({ name: articleName }, {
             '$set': {
                 comments: articleInfo.comments.concat({ username, text })
             },
@@ -54,5 +76,10 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
         const updateArticleInfo = await db.collection('articles').findOne({ name: articleName })
         res.status(200).json(updateArticleInfo)
     }, res)
-})
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
 app.listen(8000, () => console.log('Listening on port 8000'));  
